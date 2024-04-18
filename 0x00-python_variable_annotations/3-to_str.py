9da5c771ccba8cd5141ec91e91c6fc9ee96fa2cb
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-18 15:14:45 +0100

    add to_str func

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  function:
+    to_str
+"""
+import math
+
+
+def to_tsr(n: float) -> str:
+    """
+      function that returns the string representation of n
+      Args:
+        n: float
+    """
+    return str(n)
