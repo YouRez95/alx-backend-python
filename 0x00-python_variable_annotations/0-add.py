f16a2f262b954f70488341b15d665f238404d27a
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-18 15:01:08 +0100

    type annotation add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  function:
+    add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+      function that can add two floats
+      Args:
+        a: first float
+        b: second float
+    """
+    return a + b
