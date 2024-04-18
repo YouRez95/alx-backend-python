cfe1c9361c4e1c0e7f8eeeb792ab423ccd89556f
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-18 15:07:25 +0100

    type annotation for concat func

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  function:
+    concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+      function that can concatenate two strings
+      Args:
+        str1: first str
+        str2: second str
+    """
+    return '{} {}'.format(str1, str2)
