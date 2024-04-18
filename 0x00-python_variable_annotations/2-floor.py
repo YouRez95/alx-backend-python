2e0afba2add4734c0e93027834d7d3ea722efb6a
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-18 15:12:17 +0100

    type annotation for floor func

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+  function:
+    floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+      function that round numbers to the nearest integer
+      Args:
+        n: float
+    """
+    return math.floor(n)
