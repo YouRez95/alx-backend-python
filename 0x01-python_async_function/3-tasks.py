56613046f6619f2c24b2dc7a8ab9178607e03235
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-22 15:59:14 +0100

    tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+  function:
+    task_wait_random
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """
+      task_wait_random takes an integer max_delay
+      and returns a asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
