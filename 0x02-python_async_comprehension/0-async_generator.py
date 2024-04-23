641bf5aa4f7d13f203f885170c69644cdc49da13
Youness Elalouani <younesselalouani1995@gmail.com>
2024-04-23 20:25:10 +0100

    use yield

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -15,6 +15,6 @@ async def async_generator() -> AsyncGenerator[float, float]:
       each time asynchronously wait 1 second,
       then yield a random number between 0 and 10
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
